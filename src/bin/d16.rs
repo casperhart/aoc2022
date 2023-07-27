3d8852d1a60ef542ad1cce0ea41f178bd6833e6b
Casper Hart <casperhart93@gmail.com>
2023-07-26 23:24:07 -0400

    d16 part 2

--- src/bin/d16.rs
+++ src/bin/d16.rs
@@ -116,9 +116,12 @@ impl Display for Graph {
 #[derive(Debug, Clone)]
 struct State {
     graph: Rc<Graph>,
-    current_valve: usize,
-    path: Vec<usize>,
-    time: usize,
+    current_valve_1: usize,
+    current_valve_2: usize,
+    path_1: Vec<usize>,
+    path_2: Vec<usize>,
+    time_1: usize,
+    time_2: usize,
     flow: usize,
     max_flow: usize,
 }
@@ -128,9 +131,12 @@ impl State {
         let starting_value = graph.valves.iter().position(|x| x.name == "AA").unwrap();
         Self {
             graph,
-            current_valve: starting_value,
-            path: Vec::with_capacity(15),
-            time: 30,
+            current_valve_1: starting_value,
+            current_valve_2: starting_value,
+            path_1: Vec::with_capacity(15),
+            path_2: Vec::with_capacity(15),
+            time_1: 30,
+            time_2: 26,
             flow: 0,
             max_flow: 0,
         }
@@ -141,10 +147,17 @@ impl State {
             .graph
             .working_valves
             .iter()
-            .filter(|f| !self.path.contains(f))
+            .filter(|f| !self.path_1.contains(f) && !self.path_2.contains(f))
             .map(|&f| {
-                let dist = self.graph.get_distance(self.current_valve, f);
-                let remaining_time = self.time.saturating_sub(dist);
+                let dist_1 = self.graph.get_distance(self.current_valve_1, f);
+                let dist_2 = self.graph.get_distance(self.current_valve_2, f);
+
+                let remaining_time_1 = self.time_1.saturating_sub(dist_1);
+                let remaining_time_2 = self.time_2.saturating_sub(dist_2);
+
+                // this may not be the best heuristic to use
+                let remaining_time = (remaining_time_1 + remaining_time_2) / 2;
+
                 self.graph.valves[f].flow_rate * remaining_time
             })
             .sum::<usize>();
@@ -152,15 +165,28 @@ impl State {
         self.max_flow = self.flow + max_remaining_flow;
     }
 
-    fn move_to(&mut self, new_valve: usize) -> Option<()> {
-        let dist = self.graph.get_distance(self.current_valve, new_valve) + 1;
-        if dist > self.time {
+    fn move_1_to(&mut self, new_valve: usize) -> Option<()> {
+        let dist = self.graph.get_distance(self.current_valve_1, new_valve) + 1;
+        if dist > self.time_1 {
             return None;
         }
-        self.time -= dist;
-        self.path.push(new_valve);
-        self.current_valve = new_valve;
-        self.flow += self.graph.valves[new_valve].flow_rate * self.time;
+        self.time_1 -= dist;
+        self.path_1.push(new_valve);
+        self.current_valve_1 = new_valve;
+        self.flow += self.graph.valves[new_valve].flow_rate * self.time_1;
+        self.update_max_flow();
+        Some(())
+    }
+
+    fn move_2_to(&mut self, new_valve: usize) -> Option<()> {
+        let dist = self.graph.get_distance(self.current_valve_2, new_valve) + 1;
+        if dist > self.time_2 {
+            return None;
+        }
+        self.time_2 -= dist;
+        self.path_2.push(new_valve);
+        self.current_valve_2 = new_valve;
+        self.flow += self.graph.valves[new_valve].flow_rate * self.time_2;
         self.update_max_flow();
         Some(())
     }
@@ -180,20 +206,15 @@ impl PartialOrd for State {
 
 impl PartialEq for State {
     fn eq(&self, other: &Self) -> bool {
-        self.path == other.path
+        self.path_1 == other.path_1 && self.path_2 == other.path_2
     }
 }
 
 impl Eq for State {}
 
-fn main() {
+fn get_part_1(graph: Rc<Graph>) {
     let start = Instant::now();
-    let f = read_to_string("d16.txt").expect("could not read input");
-    let mut graph = Graph::new(f.as_str());
-
-    graph.floyd_warshall();
-
-    let mut state = State::new(Rc::new(graph));
+    let mut state = State::new(graph);
     let mut heap = BinaryHeap::new();
     let mut bound = 0;
     let mut best_state = state.clone();
@@ -208,10 +229,10 @@ fn main() {
             .graph
             .working_valves
             .iter()
-            .filter(|v| !s.path.contains(v))
+            .filter(|v| !s.path_1.contains(v))
         {
             let mut next_state = s.clone();
-            let res = next_state.move_to(*valve);
+            let res = next_state.move_1_to(*valve);
             if res.is_some() {
                 if next_state.flow > bound {
                     bound = next_state.flow;
@@ -224,7 +245,71 @@ fn main() {
         }
     }
 
+    println!();
     println!("Max flow: {}", best_state.flow);
-    println!("Path: {:?}", best_state.path);
+    println!("Path: {:?}", best_state.path_1);
     println!("Elapsed time: {:?}", start.elapsed());
+}
+
+fn get_part_2(graph: Rc<Graph>) {
+    let start = Instant::now();
+    let mut state = State::new(graph);
+    let mut heap = BinaryHeap::new();
+    let mut bound = 0;
+    let mut best_state = state.clone();
+
+    state.time_1 = 26;
+    state.update_max_flow();
+    heap.push(state);
+
+    while let Some(s) = heap.pop() {
+        for &valve in s
+            .graph
+            .working_valves
+            .iter()
+            .filter(|v| !s.path_1.contains(v) && !s.path_2.contains(v))
+        {
+            let mut next_state = s.clone();
+            let res = next_state.move_1_to(valve);
+            if res.is_some() {
+                if next_state.flow > bound {
+                    bound = next_state.flow;
+                    best_state = next_state.clone();
+                }
+                if next_state.max_flow > bound {
+                    heap.push(next_state);
+                }
+            }
+
+            let mut next_state = s.clone();
+            let res = next_state.move_2_to(valve);
+            if res.is_some() {
+                if next_state.flow > bound {
+                    bound = next_state.flow;
+                    best_state = next_state.clone();
+                }
+                if next_state.max_flow > bound {
+                    heap.push(next_state);
+                }
+            }
+        }
+    }
+
+    println!();
+    println!("Max flow: {}", best_state.flow);
+    println!("Path 1: {:?}", best_state.path_1);
+    println!("Path 2: {:?}", best_state.path_2);
+    println!("Elapsed time: {:?}", start.elapsed());
+}
+
+fn main() {
+    let f = read_to_string("d16.txt").expect("could not read input");
+    let mut graph = Graph::new(f.as_str());
+
+    graph.floyd_warshall();
+
+    let rc_graph = Rc::new(graph);
+
+    get_part_1(rc_graph.clone());
+    get_part_2(rc_graph);
 }
